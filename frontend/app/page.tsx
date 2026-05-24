cb1a5393d10dcdfb1e726c3bead2eeb480662028
Ismagomez11 <gomezisma366@gmail.com>
2026-05-24 18:23:00 +0200

    Primer commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Plataforma de alquiler de garajes 🚗</h1>
+      <p>Proyecto en desarrollo</p>
+    </main>
+  );
+}
