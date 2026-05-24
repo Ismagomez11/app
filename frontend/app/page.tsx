84a0c900881fe4e13f75dcbf8db64bbc3cd16cad
Ismagomez11 <gomezisma366@gmail.com>
2026-05-24 18:32:02 +0200

    Actualiza titulo

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Plataforma de alquiler de garajes 🚗</h1>
-      <p>Proyecto en desarrollo</p>
+      <h1>Mi primera app fullstack 🚀</h1>
+      <p>Proyecto en desarrollo mod</p>
     </main>
   );
 }
